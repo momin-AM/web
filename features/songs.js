ca53e5854090e75ed20394773d56b50a234a8fe9
Momin <sk4549151@gmail.com>
2025-12-21 17:08:45 +0530

    Add SONGS object with song details and lyrics

---
+++ features/songs.js
@@ -0,0 +1,30 @@
+const SONGS = {
+  yours: {
+    title: "I'm Yours(V5)",
+    audio: "songs/MyIshaaV5.mp3",
+    lyricsData: [
+      { time: 0.0, text: "O ishahaa I just wannnaaa be yours foreeeeeverrrrrr" },
+      { time: 7, text: "I love you more than my life for the eternityy" },
+      { time: 10.5, text: "I'm yours I'm yours I'm yourrrsssss" },
+      { time: 17.5, text: "I just crave for your loveeeeeeeeeee" },
+      { time: 22.6, text: "My ishamoniiii I loveee youhh sooooooooooooooooo much much much" },
+      { time: 33.2, text: "I'm yours I'm yours I'm just yourssssssssss" }
+    ]
+  },
+
+  forever: {
+    title: "I'm yours(V4.5)",
+    audio: "songs/MyIshaa.mp3",
+    lyricsData: [
+      { time: 0, text: "music... " },
+      { time: 13.3, text: "O ishaaaa i just wannnaaa be yours forever," },
+      { time: 19.1, text: "i love you more than my life for the eternity," },
+      { time: 27.2, text: "I'm yours I'm yours I'm just yours..." },
+      { time: 43, text: "music... " },
+      { time: 48.5, text: " I just crave for your love my ishamoni." },
+      { time: 54.8, text: "i love you sooooooooo much my jaan, " },
+      { time: 64, text: "I'm yours I'm yours I'm just yours." },
+      { time: 80, text: "music... " }
+    ]
+  }
+};
